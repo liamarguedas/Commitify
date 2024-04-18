593024acd4fd0a011c1f230ed89762599a3e9288
Liam <iliamftw2013@gmail.com>
2024-04-18 14:24:40 -0300

    Add( completed git_builder )

--- src/git_builder.py
+++ src/git_builder.py
@@ -7,13 +7,30 @@ import os
 
 class GitBuilder(CommentBuilder):
 
-    def __init__(self, commits=10):
+    def __init__(self, commits=10, branch="master", files="."):
+        super().__init__()
+        self.branch = branch
+        self.files = files
         self.number_of_commits = commits
 
-    def add(self, query="git add", files="."):
-        query_to_send = query + " " + files
+    def add(self):
+        query_to_send = f"git add {self.files}"
         os.system(query_to_send)
+
+    def commit_message(self):
+        return self.comment()
 
     def commit(self, message: str):
         query_to_send = f"git commit -m {message}"
         os.system(query_to_send)
+
+    def push(self):
+        query_to_send = f"git push origin {self.branch}"
+        os.system(query_to_send)
+
+    def execute(self):
+        for _ in range(self.number_of_commits):
+            _comment = self.commit_message()
+            self.add()
+            self.commit(message=_comment)
+            self.push()
