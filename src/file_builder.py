29e4ac8f072ad30015ef8be83e7e2f3f42fba9e9
Liam <iliamftw2013@gmail.com>
2024-04-18 20:45:01 -0300

    Add(random_words && File Builder)

--- src/file_builder.py
+++ src/file_builder.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 class FileBuilder:
     def __init__(self, filetype="py"):
-
-        # loading paths
+        self.filetype = filetype
         self.filepath = Path(__file__)
         self.txt_path = self.filepath / "txt"
         self.txt_file = self.txt_path / "random_words.txt"
