c35c9de2c78d740c5d23982796fc408bf6739678
Liam <iliamftw2013@gmail.com>
2024-04-18 21:33:56 -0300

    Add( file_generator files )

--- src/file_builder.py
+++ src/file_builder.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 
 class FileBuilder:
@@ -7,3 +8,16 @@ class FileBuilder:
         self.filepath = Path(__file__)
         self.txt_path = self.filepath / "txt"
         self.txt_file = self.txt_path / "random_words.txt"
+
+    def txt_file_exists(self):
+        return os.path.exists(self.txt_file)
+
+    def read_text(self):
+        if self.txt_file_exists():
+            with open(self.txt_file, "r", encoding="utf-8") as file:
+                text_file = file.readlines()
+            return [line.rstrip("\n") for line in text_file]
+        return ["file"]
+
+
+    def create_file(self)
