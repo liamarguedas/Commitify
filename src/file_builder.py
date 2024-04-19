9ea81a836250bb3a5472a0a923ccd6e9af5cd975
Liam <iliamftw2013@gmail.com>
2024-04-19 19:46:00 -0300

    Add( File gen some how working? wtf )

--- src/file_builder.py
+++ src/file_builder.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import random
 import os
 
 
@@ -8,16 +9,47 @@ class FileBuilder:
         self.filepath = Path(__file__)
         self.txt_path = self.filepath / "txt"
         self.txt_file = self.txt_path / "random_words.txt"
+        self.words = self.read_text()
+
+    @staticmethod
+    def path_exists(dirname):
+        return os.path.exists(dirname)
+
+    @staticmethod
+    def generate_extra_file(file, dirname):
+        _splited_file = file.split(".")
+        same_name_files = [
+            directory_file
+            for directory_file in os.listdir(dirname)
+            if _splited_file[0] in directory_file
+        ]
+        return f"{_splited_file[0]}{len(same_name_files) + 1}.{_splited_file[1]}"
+
+    def file_in_path(self, dirname):
+        return os.path.exists(f"{dirname}/{self.filename}")
+
+    def generate_name(self):
+        self.filename = None
+        return self.filename
 
     def txt_file_exists(self):
         return os.path.exists(self.txt_file)
 
     def read_text(self):
+
         if self.txt_file_exists():
+
             with open(self.txt_file, "r", encoding="utf-8") as file:
+
                 text_file = file.readlines()
-            return [line.rstrip("\n") for line in text_file]
+                return [line.rstrip("\n") for line in text_file]
         return ["file"]
 
-    def create_file(self):
-        pass
+    def create_file(self, dirname: str):
+
+        if self.path_exists(dirname):
+
+            file = f"{random.choice(self.words)}.{self.filetype}"
+
+            if self.file_in_path(file):
+                file = self.generate_extra_file(file)
